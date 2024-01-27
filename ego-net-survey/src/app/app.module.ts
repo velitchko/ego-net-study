b60c8d735acc015ad37fe3665e91dedadde7fff6
Velitchko Filipov <velitchko@users.noreply.github.com>
2024-01-27 13:14:48 +0100

    load all components and modules needed

--- ego-net-survey/src/app/app.module.ts
+++ ego-net-survey/src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SurveyComponent  } from 'src/components/survey/survey.component';
+import { SurveyModule } from "survey-angular-ui";
+import { MModule } from '../modules/m.module';
+import { LModule } from '../modules/l.module';
+import { RModule } from '../modules/r.module';
+import { NlModule } from '../modules/nl.module';
 
+import { AppComponent } from './app.component';
+import { SurveyComponent  } from '../components/survey/survey.component';
+import { CustomMatrixQuestionComponent } from '../components/m/m.question';
+import { CustomNodeLinkQuestionComponent } from 'src/components/nl/nl.question';
+import { CustomRadialQuestionComponent } from 'src/components/r/r.question';
+import { CustomLayeredQuestionComponent } from 'src/components/l/l.question';
 @NgModule({
   declarations: [
     AppComponent,
-    SurveyComponent
+    SurveyComponent,
+    CustomMatrixQuestionComponent,
+    CustomNodeLinkQuestionComponent,
+    CustomRadialQuestionComponent,
+    CustomLayeredQuestionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    SurveyModule,
+    MModule,
+    LModule,
+    RModule,
+    NlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
