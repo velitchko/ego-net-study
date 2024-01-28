c9eccee3731754d0611ebab528163f50db057a6b
Velitchko Filipov <velitchko@users.noreply.github.com>
2024-01-28 08:55:12 +0100

    updated backend to randomize task and approaches for each unique user

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,51 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+const egoNetApproaches = [ 'matrix', 'nodelink', 'radial', 'layered' ];
+const taskCodes = [ 't1', 't2', 't3', 't4', 't5' ];
+const taskDescriptions = new Map([
+    ['t1', 'find a node'],
+    ['t2', 'find a node\'s neighbors'],
+    ['t3', 'find a node\'s neighbors\' neighbors'],
+    ['t4', 'find a node\'s neighbors\' neighbors\' neighbors'],
+    ['t5', 'find a node\'s neighbors\' neighbors\' neighbors\' neighbors']
+]);
+
+// create a tracker for every user that visits the site
+let userTracker = new Map();
+
 app.get('/params', (req, res) => {
     // keep track of each new user 
     let user = uuid.v4();
-    let params = url.parse(req.url, true).query;
+    // grab index of last user
+    let lastUserIndex = userTracker.size - 1;
+
+    let randomEgoNetApproaches, randomTaskCode; 
+
+    // get random order of ego-net approaches
+    randomEgoNetApproaches = egoNetApproaches.sort(() => Math.random() - 0.5);
+
+    // select a random task code
+    randomTaskCode = taskCodes[Math.floor(Math.random() * taskCodes.length)];
+    
+    userTracker.set(user, {
+        egoNetApproaches: randomEgoNetApproaches,
+        taskCode: randomTaskCode,
+        taskDescription: taskDescriptions
+    });
+
+    let params = {
+        user: user,
+        egoNetApproaches: randomEgoNetApproaches,
+        taskCode: randomTaskCode,
+        taskDescription: taskDescriptions.get(randomTaskCode)
+    };
     console.log('🔍 Query parameters:', params);
+
     res.send({
         status: 200,
         message: '👍',
-        user: user
+        user: params
     });
 });
 
