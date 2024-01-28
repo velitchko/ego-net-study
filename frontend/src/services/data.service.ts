69c1ee55e96f969845a55bbeda4ae2cef97789b3
Velitchko Filipov <velitchko@users.noreply.github.com>
2024-01-28 17:35:52 +0100

    compute matrix on data parse

--- frontend/src/services/data.service.ts
+++ frontend/src/services/data.service.ts
@@ -5,6 +5,8 @@ import { DATA } from '../assets/miserables.js';
 export type Node = { id: string | number, label: string, index: number, group: number };
 export type Edge = { source: string | number, target: string | number, value: number };
 export type Cell = { source : string | number, target: string | number, value: number, x: number, y: number };
+export type NodeExt =  Node & { x: number, y: number };
+export type EdgeExt =  Edge & { source: NodeExt, target: NodeExt };
 @Injectable({
     providedIn: 'root'
 })
@@ -12,10 +14,12 @@ export type Cell = { source : string | number, target: string | number, value: n
 export class DataService {
     private nodes: Array<Node>;
     private edges: Array<Edge>;
+    private matrix: Array<Cell>;
 
     constructor() {
         this.nodes = [];
         this.edges = [];
+        this.matrix = [];
 
         // parse data from json file in assets dir
         this.parseData(DATA);
@@ -38,38 +42,7 @@ export class DataService {
     }
 
     getMatrix(): Array<Cell> {
-        const matrix: Array<Cell> = new Array<Cell>();
-
-        let edgeHash = new Map<string, any>();
-        this.edges
-            .forEach((link: Edge) => {
-                // Undirected graph - duplicate link s-t && t-s
-                let idA: string, idB: string = '';
-                if (link.source === link.target) return;
-
-                idA = `${link.source}-${link.target}`;
-                idB = `${link.target}-${link.source}`;
-
-                edgeHash.set(idA, link);
-                edgeHash.set(idB, link);
-            });
-        console.log(edgeHash);
-
-        this.nodes.forEach((source: Node) => {
-            this.nodes.forEach((target: Node) => {
-                let edge = `${source.id}-${target.id}`;              
-                    matrix.push({
-                        source: source.id,
-                        target: target.id,
-                        x: source.index,
-                        y: target.index,
-                        value: edgeHash.get(edge) ? edgeHash.get(edge).value : 0
-                    });
-                
-            });
-        });
-
-        return matrix;
+        return this.matrix.slice();
     }
 
     addNode(node: Node): void {
@@ -84,18 +57,46 @@ export class DataService {
         const nodes = data.nodes;
         const edges = data.links;
 
+        // parse nodes
         nodes.forEach((node: { id: string, group: number }, i: number) => {
             this.addNode({
                 id: node.id, label: node.id, index: i, group: node.group
             });
         });
 
+        // parse edges
+        let edgeHash = new Map<string, Edge>();
+
         edges.forEach((edge: { source: string, target: string, value: number }, i: number) => {
+            let idA: string, idB: string = '';
+            if (edge.source === edge.target) return;
+
+            idA = `${edge.source}-${edge.target}`;
+            idB = `${edge.target}-${edge.source}`;
+
+            edgeHash.set(idA, edge);
+            edgeHash.set(idB, edge);
 
             this.addEdge({
                 source: edge.source, target: edge.target, value: edge.value
             });
         });
-    }
 
+        // parse matrix
+        this.nodes.forEach((source: Node) => {
+            this.nodes.forEach((target: Node) => {
+                
+                let edge = `${source.id}-${target.id}`;
+                const edgeValue = edgeHash.get(edge)?.value;
+
+                this.matrix.push({
+                    source: source.id,
+                    target: target.id,
+                    x: source.index,
+                    y: target.index,
+                    value: edgeValue === undefined ? 0 : edgeValue
+                });
+            });
+        });   
+    }
 }
