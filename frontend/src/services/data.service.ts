be0766c8a2c4f825483090381b20707788eece2f
Velitchko Filipov <velitchko@users.noreply.github.com>
2024-02-18 12:09:37 +0100

    load and parse precomputed datasets

--- frontend/src/services/data.service.ts
+++ frontend/src/services/data.service.ts
@@ -1,38 +1,46 @@
 import { Injectable } from '@angular/core';
+import { DATA_NL_PRE } from '../assets/miserables.434827120503882069.20_precomputed.js';
 import { DATA_NL } from '../assets/miserables.434827120503882069.20.js';
 import { DATA_M } from '../assets/miserables.2499354945414561156.19.js';
+import { DATA_R_PRE } from '../assets/miserables.3848446828006397221.35_precomputed.js';
 import { DATA_R } from '../assets/miserables.3848446828006397221.35.js';
+import { DATA_L_PRE } from '../assets/miserables.8139397558749019675.37_precomputed.js';
 import { DATA_L } from '../assets/miserables.8139397558749019675.37.js';
+import { CONFIG } from '../assets/config';
 
 
-export type Node = { id: string | number, label: string, index: number, ego: string | number, hop: number, weight: number, parent: string | number };
-export type Edge = { source: string | number, target: string | number, weight: number, ego: string | number, hop: number };
-export type Cell = { source : string | number, target: string | number, weight: number, x: number, y: number, ego: string | number, hop: number };
-export type NodeExt =  Node & { x: number, y: number };
-export type EdgeExt =  Edge & { source: NodeExt, target: NodeExt };
+export type Node = { id: string | number, label: string, index: number, ego: string | number, hop: number, weight: number, parent: string | number, x?: number, y?: number };
+export type Edge = { source: string | number, target: string | number, weight: number, ego: string | number, hop: number, x1?: number, y1?: number, x2?: number, y2?: number };
+export type Cell = { source: string | number, target: string | number, weight: number, x: number, y: number, ego: string | number, hop: number };
+export type NodeExt = Node & { x: number, y: number };
+export type EdgeExt = Edge & { source: NodeExt, target: NodeExt };
 @Injectable({
     providedIn: 'root'
 })
 
 export class DataService {
-    
+
     private datasets: Map<string, any> = new Map([
-        ['nodelink', DATA_NL],
-        ['matrix', DATA_M],
-        ['layered', DATA_L],
-        ['radial', DATA_R]
+        ['nodelink', CONFIG.PRECOMPUTED ? DATA_NL_PRE : DATA_NL],
+        ['matrix', CONFIG.PRECOMPUTED ? DATA_M : DATA_M],
+        ['layered', CONFIG.PRECOMPUTED ? DATA_L_PRE : DATA_L],
+        ['radial', CONFIG.PRECOMPUTED ? DATA_R_PRE : DATA_R]
     ]);
 
     private parsedData: Map<string, { nodes: Array<Node>, edges: Array<Edge>, matrix: Array<Cell> }>;
 
     constructor() {
-        this.parsedData = new Map<string, { nodes: Array<Node>, edges: Array<Edge>, matrix: Array<Cell>}>();
+        this.parsedData = new Map<string, { nodes: Array<Node>, edges: Array<Edge>, matrix: Array<Cell> }>();
         // iterate over datasets and parse data from json file in assets dir
         this.datasets.forEach((data: any, key: string) => {
             const parsed = this.parseData(data);
-            this.parsedData.set(key, { nodes: parsed.nodes, edges: parsed.links, matrix: parsed.matrix });
+            if(!CONFIG.PRECOMPUTED) {
+                this.parsedData.set(key, { nodes: parsed.nodes, edges: parsed.links, matrix: parsed.matrix || [] });
+            } else {
+                this.parsedData.set(key, { nodes: parsed.nodes, edges: parsed.links, matrix: parsed.matrix || [] });
+            }
         });
-        
+
     }
 
     getDatasetNodes(key: string): Array<Node> {
@@ -47,7 +55,62 @@ export class DataService {
         return this.parsedData.get(key)?.matrix.slice() || [];
     }
 
-    parseData(data: any): { nodes: Array<Node>, links: Array<Edge>, matrix: Array<Cell> } {
+    parsePrecomputedData(data: any): { nodes: Array<Node>, edges: Array<Edge>, matrix?: Array<Cell> } {
+
+        const nodes = data.nodes;
+        const edges = data.links;
+
+        const parsedNodes = new Array<Node>();
+        const parsedEdges = new Array<Edge>();
+
+        nodes.forEach((node: { id: string | number, ego: string | number, hop: number, weighted: number, parent: string | number, x?: number, y?: number }, i: number) => {
+            parsedNodes.push({
+                id: node.id,
+                label: `${node.id}`,
+                index: i,
+                hop: node.hop,
+                ego: node.ego,
+                weight: node.weighted,
+                parent: node.parent,
+                x: node.x || 0,
+                y: node.y || 0
+            });
+        });
+
+        // parse edges
+        let edgeHash = new Map<string, Edge>();
+        // { source: string | number, target: string | number, weight: number, ego: string | number, hop: number };
+        edges.forEach((edge: { source: string | number, target: string | number, weight: number, ego: string | number, hop: number, x1?: number, y1?: number, x2?: number, y2?: number }, i: number) => {
+            let idA: string, idB: string = '';
+            if (edge.source === edge.target) return;
+
+            idA = `${edge.source}-${edge.target}`;
+            idB = `${edge.target}-${edge.source}`;
+
+            edgeHash.set(idA, edge);
+            edgeHash.set(idB, edge);
+
+            parsedEdges.push({
+                source: edge.source,
+                target: edge.target,
+                weight: edge.weight,
+                ego: edge.ego,
+                hop: edge.hop,
+                x1: edge.x1 || 0,
+                y1: edge.y1 || 0,
+                x2: edge.x2 || 0,
+                y2: edge.y2 || 0
+            });
+        });
+
+
+        return {
+            nodes: parsedNodes,
+            edges: parsedEdges,
+        };
+    }
+
+    parseData(data: any): { nodes: Array<Node>, links: Array<Edge>, matrix?: Array<Cell> } {
         const nodes = data.nodes;
         const edges = data.links;
 
@@ -57,15 +120,17 @@ export class DataService {
 
         // parse nodes
         // id: string | number, label: string, index: number, ego: string | number, hop: number, weight: number, parent: string | number };
-        nodes.forEach((node: { id: string | number , ego: string | number, hop: number, weighted: number, parent: string | number }, i: number) => {
+        nodes.forEach((node: { id: string | number, ego: string | number, hop: number, weighted: number, parent: string | number, x?: number, y?: number }, i: number) => {
             parsedNodes.push({
-                id: node.id, 
-                label: `${node.id}`, 
-                index: i, 
+                id: node.id,
+                label: `${node.id}`,
+                index: i,
                 hop: node.hop,
                 ego: node.ego,
                 weight: node.weighted,
-                parent: node.parent
+                parent: node.parent,
+                x: node.x || 0,
+                y: node.y || 0
             });
         });
 
@@ -83,35 +148,40 @@ export class DataService {
             edgeHash.set(idB, edge);
 
             parsedEdges.push({
-                source: edge.source, 
-                target: edge.target, 
+                source: edge.source,
+                target: edge.target,
                 weight: edge.weight,
                 ego: edge.ego,
-                hop: edge.hop
+                hop: edge.hop,
+                // get target x and y
+                x1: nodes.find((n: Node) => n.id === edge.source)?.x || 0,
+                y1: nodes.find((n: Node) => n.id === edge.source)?.y || 0,
+                x2: nodes.find((n: Node) => n.id === edge.target)?.x || 0,
+                y2: nodes.find((n: Node) => n.id === edge.target)?.y || 0
             });
         });
 
         // parse matrix
-        parsedNodes.forEach((source: Node) => {
-            parsedNodes.forEach((target: Node) => {
-                let edge = `${source.id}-${target.id}`;
+        // parsedNodes.forEach((source: Node) => {
+        //     parsedNodes.forEach((target: Node) => {
+        //         let edge = `${source.id}-${target.id}`;
 
-                parsedMatrix.push({
-                    source: source.id,
-                    target: target.id,
-                    x: source.index,
-                    y: target.index,
-                    weight: edgeHash.has(edge) ? edgeHash.get(edge)?.weight || 0 : 0,
-                    ego: edgeHash.has(edge) ? edgeHash.get(edge)?.ego || 0 : 0,
-                    hop: edgeHash.has(edge) ? edgeHash.get(edge)?.hop || 0 : 0,
-                });
-            });
-        });  
-        
+        //         parsedMatrix.push({
+        //             source: source.id,
+        //             target: target.id,
+        //             x: source.index,
+        //             y: target.index,
+        //             weight: edgeHash.has(edge) ? edgeHash.get(edge)?.weight || 0 : 0,
+        //             ego: edgeHash.has(edge) ? edgeHash.get(edge)?.ego || 0 : 0,
+        //             hop: edgeHash.has(edge) ? edgeHash.get(edge)?.hop || 0 : 0,
+        //         });
+        //     });
+        // });  
+
         return {
             nodes: parsedNodes,
             links: parsedEdges,
-            matrix: parsedMatrix
+            // matrix: parsedMatrix
         };
     }
 }
